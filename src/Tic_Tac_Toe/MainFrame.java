d6a6203e20e311ce60da620c2f79caa5e14d8c25
mariooooo3 <afrasineimario@gmail.com>
2026-02-23 17:09:34 +0200

    Missing names message warning

--- src/Tic_Tac_Toe/MainFrame.java
+++ src/Tic_Tac_Toe/MainFrame.java
@@ -13,13 +13,14 @@ public class MainFrame extends JFrame {
         setTitle("Tic Tac Toe");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(100, 100, 540, 540);
+        setLocationRelativeTo(null);
 
         cardLayout = new CardLayout();
         contentPane = new JPanel(cardLayout);
         setContentPane(contentPane);
 
         BackgroundPanel backgroundPanel =
-                new BackgroundPanel("/Tic_Tac_Toe/tic_tac_toe.png");
+                new BackgroundPanel("/tic_tac_toe.png");
 
         backgroundPanel.setLayout(new BorderLayout());
 
@@ -34,18 +35,28 @@ public class MainFrame extends JFrame {
 
         cardLayout.show(contentPane, "MENU");
 
+        ImageIcon original = new ImageIcon(getClass().getResource("/smile.png"));
+
+        Image scaled = original.getImage().getScaledInstance(
+                32, 32, Image.SCALE_SMOOTH);
+
+        ImageIcon icon = new ImageIcon(scaled);
+
+
         menuPanel.startBut.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 if (!(menuPanel.secondField.getText().isEmpty() || menuPanel.firstField.getText().isEmpty())) {
                     cardLayout.show(contentPane, "GAME");
                     gamePanel.player1.setText(menuPanel.firstField.getText() + "-X");
                     gamePanel.player2.setText(menuPanel.secondField.getText() + "-O");
-                } else
+                } else {
                     System.out.println("Enter players' names");
+                    JOptionPane.showMessageDialog(null, "Please enter players' names:",
+                            "Missing Names", JOptionPane.ERROR_MESSAGE, icon);
+                }
                 if (gamePanel.player2.getText().equals("Bot Greg-O") && gamePanel.xo.getText().equals("O Turn")) {
                     gamePanel.autoPlay();
                 }
-
             }
         });
 
@@ -56,10 +67,11 @@ public class MainFrame extends JFrame {
                     gamePanel.player1.setText(menuPanel.firstField.getText() + "-X");
                     gamePanel.player2.setText(menuPanel.secondField.getText() + "-O");
                     gamePanel.label.setText("Infinite Mode");
-                }
-                else
+                } else {
                     System.out.println("Enter players' names");
-
+                    JOptionPane.showMessageDialog(null, "Please enter players' names:",
+                            "Missing Names", JOptionPane.ERROR_MESSAGE, icon);
+                }
             }
         });
 
